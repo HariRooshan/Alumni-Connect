5ddc1b1b3a6ee3b5019979d1a729165bf2b554ea
HariRooshan <143261726+HariRooshan@users.noreply.github.com>
2025-08-07 16:31:17 +0000

    changes to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
       }}
     >
       <Container maxWidth="lg">
-        <Grid container spacing={2} alignItems="center">
+        <Grid container spacing={2} justifyContent="space-between" alignItems="center">
           {/* PSG Tech logo and label */}
           <Grid item xs={12} md="auto" display="flex" alignItems="center">
             <img src="/PSG logo.jpeg" alt="PSG Logo" style={{ width: 60, height: 60, marginRight: 10 }} />
@@ -21,30 +21,6 @@ const Footer = () => {
             </Typography>
           </Grid>
 
-          {/* Developer Info link centered */}
-          <Grid item xs={12} md>
-            <Box sx={{marginLeft:30}}>
-              <Typography variant="body1" fontWeight="bold">
-                <Link href="/developers-info" color="inherit" underline="hover">
-                  Developer Info
-                </Link>
-              </Typography>
-            </Box>
-          </Grid>
-
-          {/* Privacy Policy and Terms
-          <Grid item xs={12} md="auto">
-            <Typography variant="body1" fontWeight="bold">
-              <Link href="/privacy-policy" color="inherit" underline="hover">
-                Privacy Policy
-              </Link>
-            </Typography>
-            <Typography variant="body2">
-              <Link href="/terms-of-use" color="inherit" underline="hover">
-                Terms of Use
-              </Link>
-            </Typography>
-          </Grid> */}
           {/* Developer Info link aligned right */}
           <Grid item xs={12} md="auto">
         
